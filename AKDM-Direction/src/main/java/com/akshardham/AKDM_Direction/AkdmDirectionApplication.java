db3b9c2304731b796b9c20fee0a5c7cc60cddb96
DevPatel016 <pateldev014@gmail.com>
2024-06-03 17:58:45 -0400

    V1
    Setup p1

---
+++ AKDM-Direction/src/main/java/com/akshardham/AKDM_Direction/AkdmDirectionApplication.java
@@ -0,0 +1,13 @@
+package com.akshardham.AKDM_Direction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkdmDirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkdmDirectionApplication.class, args);
+	}
+
+}
---
+++ AKDM-Direction/src/test/java/com/akshardham/AKDM_Direction/AkdmDirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshardham.AKDM_Direction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkdmDirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
